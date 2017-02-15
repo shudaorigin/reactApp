3092591863edea4ce8a835013f18ab976cb04840
ThinkCentre <d2364227@gmail.com>
2017-02-15 11:46:32 +0800

    登录 退出 界面

--- index.android.js
+++ index.android.js
@@ -1,111 +1,28 @@
-'use strict';
-
-var RN = require('react-native');
 import React, { Component } from 'react';
-
-var {
+import {
   AppRegistry,
   StyleSheet,
   Text,
-  View,
-  Navigator,
-  TouchableOpacity,
-} = RN;
+  View
+} from 'react-native';
 
-var SCREEN_WIDTH = require('Dimensions').get('window').width;
-var BaseConfig = Navigator.SceneConfigs.FloatFromRight;
+import LoginScreen from "./screens/login/index";
+import SignupScreen from "./screens/signup/index";
 
-var CustomLeftToRightGesture = Object.assign({}, BaseConfig.gestures.pop, {
-  // Make it snap back really quickly after canceling pop
-  snapVelocity: 8,
-  // Make it so we can drag anywhere on the screen
-  edgeHitWidth: SCREEN_WIDTH,
-});
-
-var CustomSceneConfig = Object.assign({}, BaseConfig, {
-  // A very tighly wound spring will make this transition fast
-  springTension: 100,
-  springFriction: 1,
-  // Use our custom gesture defined above
-  gestures: {
-    pop: CustomLeftToRightGesture,
-  }
-});
-
-var PageOne = React.createClass({
-  _handlePress() {
-    this.props.navigator.push({id: 2,});
-  },
-
+export default class reactApp extends Component {
   render() {
     return (
-      <View style={[styles.container, {backgroundColor: 'green'}]}>
-        <Text style={styles.welcome}>Greetings!</Text>
-        <TouchableOpacity onPress={this._handlePress}>
-          <View style={{paddingVertical: 10, paddingHorizontal: 20, backgroundColor: 'black'}}>
-            <Text style={styles.welcome}>Go to page two</Text>
-          </View>
-        </TouchableOpacity>
-       </View>
-    )
-  },
-});
-
-var PageTwo = React.createClass({
-  _handlePress() {
-    this.props.navigator.pop();
-  },
-
-  render() {
-    return (
-      <View style={[styles.container, {backgroundColor: 'purple'}]}>
-        <Text style={styles.welcome}>This is page two!</Text>
-        <TouchableOpacity onPress={this._handlePress}>
-          <View style={{paddingVertical: 10, paddingHorizontal: 20, backgroundColor: 'black'}}>
-            <Text style={styles.welcome}>Go back</Text>
-          </View>
-        </TouchableOpacity>
-       </View>
-    )
-  },
-});
-
-var reactApp = React.createClass({
-  _renderScene(route, navigator) {
-    if (route.id === 1) {
-      return <PageOne navigator={navigator} />
-    } else if (route.id === 2) {
-      return <PageTwo navigator={navigator} />
-    }
-  },
-
-  _configureScene(route) {
-    return CustomSceneConfig;
-  },
-
-  render() {
-    return (
-      <Navigator
-        initialRoute={{id: 1, }}
-        renderScene={this._renderScene}
-        configureScene={this._configureScene} />
+      <View style={styles.container}>
+        <LoginScreen />
+      </View>
     );
   }
-});
+}
 
-var styles = StyleSheet.create({
+const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-    color: 'white',
-  },
+  }
 });
 
 AppRegistry.registerComponent('reactApp', () => reactApp);
