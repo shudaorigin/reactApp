be75fb39a7963ba7c7fdd648487be96238db9e31
ThinkCentre <d2364227@gmail.com>
2017-02-15 11:24:30 +0800

    多视图切换

--- index.android.js
+++ index.android.js
@@ -1,301 +1,111 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-
 'use strict';
+
+var RN = require('react-native');
 import React, { Component } from 'react';
-import {
+
+var {
   AppRegistry,
   StyleSheet,
   Text,
-  ListView,
-  TextInput ,
   View,
-  Button,
-  Image,
+  Navigator,
   TouchableOpacity,
-  TouchableHighlight,
-  ScrollView,
-} from 'react-native';
+} = RN;
 
-var Dimensions = require('Dimensions');
-import ModalDropdown from 'react-native-modal-dropdown';
-import Menu, { MenuContext, MenuOptions, MenuOption, MenuTrigger } from 'react-native-menu';
+var SCREEN_WIDTH = require('Dimensions').get('window').width;
+var BaseConfig = Navigator.SceneConfigs.FloatFromRight;
 
-var TwoSideMenus = require('./component/TwoSideMenus');
+var CustomLeftToRightGesture = Object.assign({}, BaseConfig.gestures.pop, {
+  // Make it snap back really quickly after canceling pop
+  snapVelocity: 8,
+  // Make it so we can drag anywhere on the screen
+  edgeHitWidth: SCREEN_WIDTH,
+});
 
-
-//import SideMenu from 'react-native-side-menu';
-//const SideMenu = require('react-native-side-menu');
-//const MenuItem = require('./component/Menu.js');
-
-var screenW = Dimensions.get('window').width;
-var screenH = Dimensions.get('window').height;
-const onButtonPress = () => { alert('Button has been pressed!'); };
-
-const TopNavigation = () => (
-  <View style={{ padding: 10, flexDirection: 'row', backgroundColor: 'pink' }}>
-    <View style={{ flex: 1 }}><Text>My App</Text></View>
-    <Menu onSelect={(value) => alert(`User selected the number ${value}`)}>
-      <MenuTrigger>
-        <Text style={{ fontSize: 20 }}>12312312</Text>
-      </MenuTrigger>
-      <MenuOptions>
-        <MenuOption value={1}>
-          <Text>One</Text>
-        </MenuOption>
-        <MenuOption value={2}>
-          <Text>Two</Text>
-        </MenuOption>
-      </MenuOptions>
-    </Menu>
-  </View>
-);
-
-var BoxStyles = StyleSheet.create({
-    "height50": {
-        height : 50,
-    },
-
-    "height400": {
-        height : screenH,
-    },
-
-    "width400" : {
-        width : screenW,
-    },
-
-    "bgred" : {
-        backgroundColor : "#6AC5AC",
-    },
-
-    "box" : {
-        flexDirection : "column",
-        flex: 1,
-        position : "relative",
-    },
-
-    "centerView" : {
-        flexDirection: "row",
-        flex : 1,
-        justifyContent : "space-between",
-    },
-
-    "label" : {
-        top: 0,
-        left: 0,
-        paddingTop : 0,
-        paddingRight: 3,
-        paddingBottom : 3,
-        position : "absolute",
-        backgroundColor : "#FDC72F",
-    },
-
-    "top" : {
-        justifyContent : "center",
-        alignItems : "center",
-    },
-
-    "bottom" : {
-        justifyContent: "center",
-        alignItems : "center",
-    },
-
-    "right" : {
-        justifyContent:"space-around",
-        width : 50,
-        alignItems : "center",
-    },
-
-    "left" : {
-        justifyContent: "space-around",
-        width: 50,
-        alignItems: "center",
-    },
-
-    "margginBox" : {
-        "position" : "absolute",
-       // "top" : 100,
-      //  "paddingLeft" : 7,
-      //  "paddingRight" : 7,
-
-    },
-	dropdown_6_image: {
-		width: 40,
-		height: 40,
-	},
-	backgroundImage: {
-        flex: 1,
-		// remove width and height to override fixed static size
-		width: null,
-		height: null,
-    },
-})
-
-var Box = React.createClass({
-    render:function(){
-        return(
-        <View style = {[BoxStyles.box,BoxStyles[this.props.width],BoxStyles[this.props.height]]}>
-          <Text>top</Text>
-        </View>
-    )
+var CustomSceneConfig = Object.assign({}, BaseConfig, {
+  // A very tighly wound spring will make this transition fast
+  springTension: 100,
+  springFriction: 1,
+  // Use our custom gesture defined above
+  gestures: {
+    pop: CustomLeftToRightGesture,
   }
+});
 
-})
-var MargginBox = React.createClass({
-    render : function(){
-        return (
-        <View style={[BoxStyles.margginBox]}>
-            <Text>top</Text> 
-        </View>
-        )
-    }
-
-})
-
-let dropdown_6_icon =  require('./images/flower.png');
-class MenuSideItem extends Component {
-  constructor(props) {
-    super(props);
-  }
-
-  menuMightOpen() {
-    console.log('left menu might open');
-  }
-
-  menuDidOpen() {
-    console.log('left menu did open');
-  }
-
-  menuDidClose() {
-    console.log('left menu did close');
-  }
+var PageOne = React.createClass({
+  _handlePress() {
+    this.props.navigator.push({id: 2,});
+  },
 
   render() {
     return (
-      <ScrollView style={{ flex: 1, backgroundColor: this.props.color, height: 2000 }}/>
+      <View style={[styles.container, {backgroundColor: 'green'}]}>
+        <Text style={styles.welcome}>Greetings!</Text>
+        <TouchableOpacity onPress={this._handlePress}>
+          <View style={{paddingVertical: 10, paddingHorizontal: 20, backgroundColor: 'black'}}>
+            <Text style={styles.welcome}>Go to page two</Text>
+          </View>
+        </TouchableOpacity>
+       </View>
+    )
+  },
+});
+
+var PageTwo = React.createClass({
+  _handlePress() {
+    this.props.navigator.pop();
+  },
+
+  render() {
+    return (
+      <View style={[styles.container, {backgroundColor: 'purple'}]}>
+        <Text style={styles.welcome}>This is page two!</Text>
+        <TouchableOpacity onPress={this._handlePress}>
+          <View style={{paddingVertical: 10, paddingHorizontal: 20, backgroundColor: 'black'}}>
+            <Text style={styles.welcome}>Go back</Text>
+          </View>
+        </TouchableOpacity>
+       </View>
+    )
+  },
+});
+
+var reactApp = React.createClass({
+  _renderScene(route, navigator) {
+    if (route.id === 1) {
+      return <PageOne navigator={navigator} />
+    } else if (route.id === 2) {
+      return <PageTwo navigator={navigator} />
+    }
+  },
+
+  _configureScene(route) {
+    return CustomSceneConfig;
+  },
+
+  render() {
+    return (
+      <Navigator
+        initialRoute={{id: 1, }}
+        renderScene={this._renderScene}
+        configureScene={this._configureScene} />
     );
   }
-}
+});
 
-class BackgroundImage extends Component {
-  render() {
-    const {source, children, style, ...props} = this.props
-    return (
-      <Image source={ source }
-             style={ { flex: 1, width: null, height: null, ...style } }
-             {...props}>
-        { children }
-      </Image>
-    )
-  }
-};
-
-BackgroundImage.propTypes = {
-  source: React.PropTypes.object,
-  children: React.PropTypes.object,
-  style: React.PropTypes.object
-};
-
- 
-var reactApp = React.createClass({
-	/*
-  render: function() {
-    return (
-        <MargginBox></MargginBox>
-    )}
-	*/
-		
-	 componentDidMount() {
-		 /*
-		this.refs.menu.moreLifeCycles(this.refs.leftMenu, this.refs.rightMenu);
-	 	setTimeout(() => {
-		  this.refs.menu.openMenu('left');
-	 
-		  setTimeout(() => {
-			this.refs.menu.closeMenu();
-		  }, 2000);
-	 
-		}, 2000);
-		*/
-	},
-	 
-	
-    getInitialState(){
-        // 初始化数据源(rowHasChanged是优化的一种手段，只有当r1 !== r2的时候才会重新渲染)
-        var ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
-        return{
-            // 给dataSource传递一组 数组
-            dataSource: ds.cloneWithRows(['内容0', '内容1', '内容2'])
-        }
-    },
-	 // 返回一个Item
-    renderRow(rowData,sectionID,rowID) {
-        return(
-            // 实例化Item
-            <View>
-                <Text style={{backgroundColor:'red', height:44}}>内容{rowData},在第{sectionID}组第{rowID}行</Text>
-            </View>
-        )
-    },
-	
-	render() {
-		return (
-		/*
-		 <TwoSideMenus
-			ref="menu"
-			leftMenu={<MenuSideItem ref="leftMenu"  color="yellow" style={{borderWidth:1,borderColor:'red'}}/>}
-			rightMenu={<MenuSideItem ref="rightMenu" color="red"/>}>
-		 </TwoSideMenus>
-		 <Text style={{width:40,marginTop:5,}}>发布</Text> 
-		 
-		 */
-		<BackgroundImage source={ { uri: "http://scimg.158sucai.com/allimg/160702/105-160F2162TJ56.jpg" } }>
- 
-					  <View style={{
-						flex: 1,
-						flexDirection: 'column',
-						justifyContent: 'space-between',
-					  }}>
-					  
-					   <MenuContext style={{ flex: 1 }}>
-							<TopNavigation/>
-							<View style={{ flex: 1, justifyContent: 'center', alignItems: 'center',backgroundColor:'white' }}>
-							   <Text>Hello!12333</Text>
-							</View>
-						   </MenuContext>
-						   
-						<View style={{height: 50,borderBottomWidth:1, backgroundColor: 'powderblue',alignItems: 'center',justifyContent: 'center'}} >	
-						   
-							<ModalDropdown defaultValue="菜单"  options={['option 1', 'option 2']}>
-							  <Image style={BoxStyles.dropdown_6_image} source={dropdown_6_icon}	  />
-						   </ModalDropdown>			  
-			  
-						</View>			  
-						<ListView style={{backgroundColor:'white',flex:1}}             
-							dataSource={this.state.dataSource}
-							renderRow={this.renderRow}
-						/>
-							
-						<View style={{ height: 50, backgroundColor: '#fffef9',borderTopColor:'#CCCCCC',borderTopWidth:1,flexDirection: 'row',}} >
-						    <View style={{ flex:1, flexDirection: 'row',alignItems:'center',justifyContent:'center',paddingLeft:5,paddingRight:5,}}>
-							 <Image source={require('./images/flower.png')} style={{width:30,height:30,}} />                            
- 							 <TextInput  style={{ flex:1,borderColor: 'white',}}
-								onChangeText={(text) => this.setState({text})}
-								value={this.state.text} >
-							   </TextInput > 
-							   <Button onPress={onButtonPress} title="发布" color="green"  style={{width:30,height:30,}}
-							   />
-							</View>
-						</View>
-					</View>	   
-		
-	       </BackgroundImage>
-		);
-	}
-})
-
+var styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F5FCFF',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+    color: 'white',
+  },
+});
 
 AppRegistry.registerComponent('reactApp', () => reactApp);
